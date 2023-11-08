be5b50a0ae899d2a293113dd38616089d2ca02a8
Jake A Vissicchio <60723403+vissicchioj@users.noreply.github.com>
2023-11-07 20:13:08 -0500

    css

--- react-info-page/react-info-page/src/index.js
+++ react-info-page/react-info-page/src/index.js
@@ -9,28 +9,45 @@ function Page() {
     return (
         <div>
             <Header />
-            <h1>Reasons I am excited to learn React:</h1>
-            <ol>
-                <li>Making dynamic webpages is an awesome skill</li>
-                <li>I want money</li>
-                <li>I want to be employed</li>
-            </ol>
-            <footer>
-                <small>@ 2023 Vissicchio Development. All rights reserved.</small>
-            </footer>
+            <MainContent />
+            <Footer />
         </div>
     )
 }
 
 function Header() {
     return (
-        <div>
-            <header>
-                <nav>
-                    <img src = {require("./logo.png")} width = "40px" />
-                </nav>
-            </header>
+        <header className="head">
+            <nav className="nav">
+                <img src = {require("./logo.png")} className = "logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div className="main">
+            <h1>Reasons I am excited to learn React:</h1>
+            <ol>
+                <li>Making dynamic webpages is an awesome skill</li>
+                <li>I want money</li>
+                <li>I want to be employed</li>
+            </ol>
         </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="foot">
+            <small>@ 2023 Vissicchio Development. All rights reserved.</small>
+        </footer>
     )
 }
 
