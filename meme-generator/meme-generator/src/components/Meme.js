c40c2a9ca824769cc89597060e9ecf813f522e10
Jake A Vissicchio <60723403+vissicchioj@users.noreply.github.com>
2024-01-31 20:11:59 -0500

    random meme

--- meme-generator/meme-generator/src/components/Meme.js
+++ meme-generator/meme-generator/src/components/Meme.js
@@ -1,5 +1,18 @@
+import React from "react"
+import memesData from "../memesData.js";
 
 function Meme() {
+    let meme
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+        return url
+    }
+
+
     return (
         <main>
             <form className = "form">
@@ -13,7 +26,13 @@ function Meme() {
                     placeholder = "Bottom Text"
                     className = "form-input"
                 />
-                <button className = "form-button">Get a new meme image 🖼️</button>
+                <button 
+                    className = "form-button"
+                    type="button"
+                    onClick = {getMemeImage}
+                >
+                    Get a new meme image 🖼️
+                </button>
             </form>
         </main>
     )
